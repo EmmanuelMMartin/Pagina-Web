923462c86b622098fe4c9da3e5571b7ae937b273
Emmanuel Martin <manymm00@gmail.com>
2024-05-17 03:08:11 -0600

    consultastest2

--- consultas.php
+++ consultas.php
@@ -39,4 +39,41 @@ class Consultas
             return false;
         }
     }
+
+    public function obtenerCasas()
+    {
+        $sql = "SELECT * FROM casas";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } else {
+            return [];
+        }
+    }
+
+    public function imprimeBonito($objImprimir)
+    {
+        echo "<pre>";
+        print_r($objImprimir);
+        echo "</pre>";
+    }
+
+    public function __destruct()
+    {
+        $this->conn->close();
+    }
 }
+
+// Uso de la clase
+$consultas = new Consultas();
+$casas = $consultas->obtenerCasas();
+$consultas->imprimeBonito($casas);
+
+$usuarioValido = $consultas->validarUsuario('Javi', '123456');
+if ($usuarioValido) {
+    echo "Usuario válido";
+} else {
+    echo "Usuario no válido";
+}
+?>
